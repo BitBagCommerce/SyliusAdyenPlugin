a569fe32a7638643ba0bd7daaf3cd01012b25798
Przemyslaw Pawliczuk <przemyslaw.pawliczuk@bitbag.pl>
2021-07-06 14:22:20 +0200

    fixed unit tests

--- tests/Unit/Validator/AdyenCredentialsValidatorTest.php
+++ tests/Unit/Validator/AdyenCredentialsValidatorTest.php
@@ -4,31 +4,39 @@ declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusAdyenPlugin\Unit\Validator;
 
+use Adyen\HttpClient\ClientInterface;
 use BitBag\SyliusAdyenPlugin\Client\AdyenClientInterface;
+use BitBag\SyliusAdyenPlugin\Client\AdyenTransportFactory;
 use BitBag\SyliusAdyenPlugin\Validator\Constraint\AdyenCredentials;
 use BitBag\SyliusAdyenPlugin\Validator\Constraint\AdyenCredentialsValidator;
 use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
 class AdyenCredentialsValidatorTest extends ConstraintValidatorTestCase
 {
-    /** @var AdyenClientInterface */
+    /** @var ClientInterface */
     private $client;
 
     protected function setUp(): void
     {
-        $this->client = $this->createMock(AdyenClientInterface::class);
+        $this->client = $this->createMock(ClientInterface::class);
         parent::setUp();
     }
 
     protected function createValidator(): AdyenCredentialsValidator
     {
-        return new AdyenCredentialsValidator($this->client);
+        return new AdyenCredentialsValidator(
+            new AdyenTransportFactory($this->client)
+        );
     }
 
     public function testAffirmative()
     {
         $constraint = new AdyenCredentials();
-        $this->validator->validate([], $constraint);
+        $this->validator->validate([
+            'environment' => AdyenClientInterface::TEST_ENVIRONMENT,
+            'merchantAccount' => 'mer',
+            'apiKey' => 'api'
+        ], $constraint);
 
         $this->assertNoViolation();
     }
