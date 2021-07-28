2342ef60bdaa3042b953abe3b8078c33e253bcb5
Przemyslaw Pawliczuk <przemyslaw.pawliczuk@bitbag.pl>
2021-07-28 12:29:14 +0200

    header comment

--- src/Resources/public/js/checkout.js
+++ src/Resources/public/js/checkout.js
@@ -1,3 +1,10 @@
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 document.addEventListener('DOMContentLoaded', (e) => {
 
     let $form = document.querySelector('form[name=sylius_checkout_select_payment]');
--- src/Resources/public/js/dropin.js
+++ src/Resources/public/js/dropin.js
@@ -1,3 +1,10 @@
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 (() => {
     let instantiate = (container) => {
 
