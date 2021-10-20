e9eae4451599b5bd5d7eb40b3ef33da233a385b2
Przemyslaw Pawliczuk <przemyslaw.pawliczuk@bitbag.pl>
2021-10-20 13:57:46 +0200

    fixed payloads and templates

--- src/Client/ClientPayloadFactory.php
+++ src/Client/ClientPayloadFactory.php
@@ -72,11 +72,7 @@ final class ClientPayloadFactory implements ClientPayloadFactoryInterface
         array $receivedPayload,
         ?AdyenTokenInterface $adyenToken = null
     ): array {
-        $payload = [
-            'details' => $receivedPayload,
-        ];
-
-        $payload = $this->enableOneOffPayment($payload, $adyenToken);
+        $payload = $this->enableOneOffPayment($receivedPayload, $adyenToken);
         $payload = $this->versionResolver->appendVersionConstraints($payload);
 
         return $payload;
@@ -89,6 +85,11 @@ final class ClientPayloadFactory implements ClientPayloadFactoryInterface
         OrderInterface $order,
         ?AdyenTokenInterface $adyenToken = null
     ): array {
+        $countryCode = $order->getBillingAddress() !== null
+            ? (string)$order->getBillingAddress()->getCountryCode()
+            : null
+        ;
+
         $payload = [
             'amount' => [
                 'value' => $order->getTotal(),
@@ -102,6 +103,7 @@ final class ClientPayloadFactory implements ClientPayloadFactoryInterface
             ],
             'channel' => 'web',
             'origin' => $this->getOrigin($url),
+            'countryCode' => $countryCode
         ];
 
         $payload = $this->filterArray($receivedPayload, [
