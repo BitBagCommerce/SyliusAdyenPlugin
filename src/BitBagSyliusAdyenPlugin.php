da2b36ec586804eab50ea3e1744ef4e3412f94bf
Przemyslaw Pawliczuk <przemyslaw.pawliczuk@bitbag.pl>
2021-10-26 17:40:15 +0200

    PoC

--- src/BitBagSyliusAdyenPlugin.php
+++ src/BitBagSyliusAdyenPlugin.php
@@ -11,11 +11,19 @@ declare(strict_types=1);
 namespace BitBag\SyliusAdyenPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class BitBagSyliusAdyenPlugin extends Bundle
 {
+    const TAG_FALLBACK = [
+        'sylius.command_bus' => 'sylius_default.bus',
+        'sylius.event_bus' => 'sylius_event.bus'
+    ];
+
     use SyliusPluginTrait;
 
     public function getContainerExtension(): ?ExtensionInterface
@@ -24,4 +32,28 @@ final class BitBagSyliusAdyenPlugin extends Bundle
 
         return $this->containerExtension !== false ? $this->containerExtension : null;
     }
+
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new class implements CompilerPassInterface{
+
+            public function process(ContainerBuilder $container)
+            {
+                $buses = array_keys($container->findTaggedServiceIds('messenger.bus'));
+
+                $handlers = $container->findTaggedServiceIds('bitbag.sylius_adyen_plugin.command_bus');
+                $container->setAlias('bitbag.sylius_adyen_plugin.command_bus', 'sylius_default.bus');
+
+                foreach ($handlers as $handler=>$tagData) {
+                    $def = $container->findDefinition($handler);
+                    $busName = $tagData[0]['bus'] ?? 'sylius.command_bus';
+                    $def->addTag('messenger.message_handler', [
+                        'bus' => in_array($busName, $buses, true) ? $busName : BitBagSyliusAdyenPlugin::TAG_FALLBACK[$busName]
+                    ]);
+                }
+            }
+        }, PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
+    }
+
+
 }
