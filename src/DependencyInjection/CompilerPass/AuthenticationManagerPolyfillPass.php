79ca468d4dd30acd1b10b53f5173bbf607f949f5
pptasinski <przemyslaw.ptasinski@gmail.com>
2021-11-30 21:51:15 +0100

    feat: polyfill pass for authentication manager

---
+++ src/DependencyInjection/CompilerPass/AuthenticationManagerPolyfillPass.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+namespace BitBag\SyliusAdyenPlugin\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class AuthenticationManagerPolyfillPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (
+            $container->has('security.authentication_manager') === false
+            &&
+            $container->has('security.authentication.manager') === true
+        ) {
+            $container->setAlias('security.authentication_manager', 'security.authentication.manager');
+        }
+    }
+}
