e2b12617605a03104e42536a81adcf4f4bcc735a
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-02-17 14:40:09 +0100

    Add MockerContainer

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -73,7 +73,7 @@ final class Kernel extends BaseKernel
 
     protected function getContainerBaseClass(): string
     {
-        if ($this->isTestEnvironment()) {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
             return MockerContainer::class;
         }
 
