6340e3fc07f07c9d63654b57edd2d6948093a553
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-03-09 15:20:29 +0100

    Added missing Wechatpay payment method to allowed payment method list

--- src/Client/ClientPayloadFactory.php
+++ src/Client/ClientPayloadFactory.php
@@ -56,6 +56,9 @@ final class ClientPayloadFactory implements ClientPayloadFactoryInterface
         'ach',
         'directdebit_GB',
         'ratepay_directdebit',
+        'wechatpayWeb',
+        'wechatpaySDK',
+        'wechatpayQR',
     ];
 
     public function __construct(
