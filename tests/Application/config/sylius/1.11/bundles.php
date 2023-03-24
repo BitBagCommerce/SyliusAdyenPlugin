25a2498c9f34290499442a275be2bab9ccf469a5
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-24 14:17:18 +0100

    Upgraded to sylius 1.12

--- tests/Application/config/sylius/1.11/bundles.php
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 return [
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true]
 ];
