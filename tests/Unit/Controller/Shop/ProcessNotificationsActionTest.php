2c9ef25da3b50f20bace149992b9bb00b462cb05
Enrique Barbeito García <ebarbeito@gmail.com>
2024-09-08 10:39:10 +0200

    Perform coding standard checks

--- tests/Unit/Controller/Shop/ProcessNotificationsActionTest.php
+++ tests/Unit/Controller/Shop/ProcessNotificationsActionTest.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusAdyenPlugin\Unit\Controller\Shop;
@@ -19,7 +27,7 @@ final class ProcessNotificationsActionTest extends TestCase
             $this->createMock(DispatcherInterface::class),
             $this->createMock(NotificationToCommandResolverInterface::class),
             $this->createMock(NotificationResolverInterface::class),
-            new  NullLogger()
+            new  NullLogger(),
         );
 
         $response = $action('dummy-code', RequestMother::createDummy());
